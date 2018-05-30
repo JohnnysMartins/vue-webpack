e7ea44c29c86ebfd8e2eb94546d45252057012e0
johnnys.martins <johnnys.martins@mv.com.br>
2018-05-30 16:52:17 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <h1>Lets go somewhere</h1>
+    <hr>
+    <a href="/">home</a>
+    <a href="/users">users</a>
+    <hr>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {}
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/Users.vue
@@ -0,0 +1,13 @@
+<template>
+    <h2>Users Overview</h2>
+</template>
+
+<script>
+    export default {
+        name: "Users"
+    }
+</script>
+
+<style scoped>
+
+</style>
